d28efa2c5fa07e7c933f17df08b32c957347a448
Randy <drbugsmn@gmail.com>
2017-01-24 19:37:23 -0600

    Initial checkin

---
+++ src/test/java/com/dood/amqp/AmqpSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dood.amqp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmqpSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
